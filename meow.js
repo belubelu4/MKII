923a5931b53ff21eb0f98f286cf72c45267c4461
ryokr <ryohuy2410@gmail.com>
2024-12-14 18:54:23 +0700

    ✦ From Ryo.o With Luv 🌻

--- meow.js
+++ meow.js
@@ -1,8 +1,1 @@
-const { ShardingManager } = require('discord.js')
-const config = require('./Src/App/config')
-
-const manager = new ShardingManager('./Src/App/index.js', { token: config.token })
-manager.on('shardCreate', (shard) => console.log('✔️    ✦ 🌑 Launched shard -- ' + shard.id))
-manager.spawn()
-
-//require('./Src/App')
+require('./Src/App')
