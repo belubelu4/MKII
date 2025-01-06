b5e92897f4fe146cb13f1ea04915e39733db548e
ryokr <ryohuy2410@gmail.com>
2025-01-06 22:58:16 +0700

    ✦ From Ryo.o With Luv 🌻

--- Src/Events/Client/voiceStateUpdate.js
+++ Src/Events/Client/voiceStateUpdate.js
@@ -4,16 +4,44 @@ const Event = require('../../Structures/Event')
 module.exports = class VoiceStateUpdate extends Event {
    constructor(client) {
       super(client, 'voiceStateUpdate')
+      this.leaveTimers = new Map()
    }
 
    async run(oldState, newState) {
-      try {
-         if (this.config.autoJoin) autoJoin(this.client, oldState, newState)
+      updateDescription(this.client, oldState, newState)
 
-         updateDescription(this.client, oldState, newState)
-         if(isMainGuild(newState.guild.id, this.config.guild.id)) unDeaf(this.player.getQueue(newState.guild.id))
-      } catch (error) {
-         console.log(`❌ ✦ [At ${__filename}]`, error)
+      if (this.config.autoJoin) autoJoin(this.client, oldState, newState)
+      if (isMainGuild(newState.guild.id, this.config.guild.id)) unDeaf(this.player.getQueue(newState.guild.id))
+
+      const botChannel = oldState.guild.members.me.voice.channel
+      if (botChannel?.id === oldState.channelId) {
+         const hasHumans = botChannel.members.some((member) => !member.user.bot)
+
+         if (hasHumans) {
+            this.clearLeaveTimer(botChannel.id)
+         } else if (!this.leaveTimers.has(botChannel.id)) {
+            this.setLeaveTimer(botChannel.id, () => {
+               const stillEmpty = !botChannel.members.some((member) => !member.user.bot)
+               if (stillEmpty) this.player.voices.leave(oldState.guild.id)
+            })
+         }
+      }
+   }
+
+   setLeaveTimer(channelId, callback) {
+      const timer = setTimeout(() => {
+         callback()
+         this.leaveTimers.delete(channelId)
+      }, 30000)
+
+      this.leaveTimers.set(channelId, timer)
+   }
+
+   clearLeaveTimer(channelId) {
+      const timer = this.leaveTimers.get(channelId)
+      if (timer) {
+         clearTimeout(timer)
+         this.leaveTimers.delete(channelId)
       }
    }
 }
