5c967a9ce8cb728cbc641f6fd7fe820121153554
ryokr <ryohuy2410@gmail.com>
2025-01-08 17:29:18 +0700

    ✦ From Ryo.o With Luv 🌻

--- Src/Commands/Admin/stat.js
+++ Src/Commands/Admin/stat.js
@@ -10,13 +10,11 @@ module.exports = class Stat extends Command {
 
    async run(interaction, embed) {
       embed.setDescription(
-         `**
-         ✦ Guild Count: \`${this.client.guilds.cache.size}\`
+         `**✦ Guild Count: \`${this.client.guilds.cache.size}\`
          ✦ Connected Voice: \`${this.client.voice.adapters.size}\`
          ✦ Operation Time: <t:${Math.floor(Number(Date.now() - this.client.uptime) / 1000)}:R>
          ✦ Ping: \`${this.client.ws.ping} MS\`
-         ✦ Memory Usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`
-         **`
+         ✦ Memory Usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`**`
       )
 
       this.removeMessage(await interaction.editReply({ embeds: [embed] }), 60000)
