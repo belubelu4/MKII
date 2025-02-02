4a446664d866dcbbde0ee7df7390bc9f590090cc
ryokr <ryohuy2410@gmail.com>
2025-02-02 16:55:40 +0700

    ✦ From Ryo.o With Luv 🌻

--- Src/Events/Client/voiceStateUpdate.js
+++ Src/Events/Client/voiceStateUpdate.js
@@ -11,13 +11,13 @@ module.exports = class VoiceStateUpdate extends Event {
 
       if (this.config.autoJoin) autoJoin(this.client, oldState, newState)
 
-      // if (isMainGuild(newState.guild.id, this.config.guild.id)) {
-      //    unDeaf(this.player.getQueue(newState.guild.id))
-      // } else {
-      //    const voiceChanel = oldState.guild.members.me.voice.channel
-      //    if (voiceChanel?.id === oldState.channelId && !voiceChanel.members.some((member) => !member.user.bot)) {
-      //       setTimeout(() => this.player.voices.leave(oldState.guild.id), 5000)
-      //    }
-      // }
+      if (isMainGuild(newState.guild.id, this.config.guild.id)) {
+         unDeaf(this.player.getQueue(newState.guild.id))
+      } else {
+         const voiceChanel = oldState.guild.members.me.voice.channel
+         if (voiceChanel?.id === oldState.channelId && !voiceChanel.members.some((member) => !member.user.bot)) {
+            setTimeout(() => this.player.voices.leave(oldState.guild.id), 5000)
+         }
+      }
    }
 }
